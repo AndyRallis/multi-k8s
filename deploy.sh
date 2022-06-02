8f94cce55a51649c6c95a5d57b88d024b2cfac27
andy.rallis <andy.rallis@evadata.com>
2022-06-02 14:07:40 -0600

    Add second -t argument

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t nashno9/multi-client:latest nashno9/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t nashno9/multi-server:latest nashno9/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t nashno9/multi-worker:latest nashno9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t nashno9/multi-client:latest -t nashno9/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nashno9/multi-server:latest -t nashno9/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nashno9/multi-worker:latest -t nashno9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push nashno9/multi-client:latest
 docker push nashno9/multi-server:latest
