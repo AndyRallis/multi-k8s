a2cabbad67a79e06cc804e89107f7640ca85454a
andy.rallis <andy.rallis@evadata.com>
2022-06-02 13:47:39 -0600

    Correct typo

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t nashno9/multi-client:latest nashno9/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t nashno9/multi-server:latest nashno9/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t nashno9/multi-worker:latest nashno9/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push nashno9/multi-client:latest
+docker push nashno9/multi-server:latest
+docker push nashno9/multi-worker:latest
+
+docker push nashno9/multi-client:$SHA
+docker push nashno9/multi-server:$SHA
+docker push nashno9/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+#kubectl set image deployments/server-deployment server=nashno9/multi-server:$SHA
+kubectl set image deployments/client-deployment server=nashno9/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=nashno9/multi-worker:$SHA
